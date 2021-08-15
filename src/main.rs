3c726575f6cb553671071a74ffa011c3857bf0d6
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-08-15 21:04:12 +0900

    feat: read rss

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rss::{Channel, Item};
+
+/// RSSのデータを格納する
+struct RssData<'a> {
+    title:Option<&'a String>,
+    description: Option<&'a String>,
+    link: Option<&'a String>,
+}
+
+#[tokio::main]
+async fn main() {
+    let rss_data = read_rss().await.unwrap();
+    let data_list:Vec<RssData>  = rss_data.items.iter().map(|item| RssData{title: item.title.as_ref(), description: item.description.as_ref(), link: item.link.as_ref()}).collect();
+    for item in data_list.iter() {
+        println!("title: {:?}", item.title.unwrap());
+        println!("description: {:?}", item.description.unwrap());
+        println!("link: {:?}", item.link.unwrap());
+    }
+
+}
+
+/// rss読む
+async fn read_rss() -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get("https://zenn.dev/feed")
+        .await?
+        .bytes()
+        .await?;
+    let channel = Channel::read_from(&content[..])?;
+    Ok(channel)
 }
